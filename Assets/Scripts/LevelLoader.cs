278d436ca040afb6ce3db454db9450a07123cf9d
Gladwinnnn <gladwin1503@hotmail.com>
2020-06-07 21:43:54 +0800

    Initial commit

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("Game Scene");
+    }
+
+    public void LoadOptionMenu()
+    {
+        SceneManager.LoadScene("Option Menu");
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("Start Menu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
